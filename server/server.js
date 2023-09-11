8bd6ade943f6b8d18ea80d63f2a00e43969b6756
Austin W <auswaller@gmail.com>
2023-09-11 10:59:42 -0400

    added model and schema for user, added authentication middleware

--- server/server.js
+++ server/server.js
@@ -3,7 +3,6 @@ const { ApolloServer } = require('@apollo/server');
 const { expressMiddleware } = require('@apollo/server/express4');
 const path = require('path');
 const { authMiddleware } = require('./utils/auth');
-
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
@@ -14,7 +13,7 @@ const server = new ApolloServer({
   resolvers,
 });
 
-// Create a new instance of an Apollo server with the GraphQL schema
+
 const startApolloServer = async () => {
   await server.start();
 
@@ -41,5 +40,5 @@ const startApolloServer = async () => {
   });
 };
 
-// Call the async function to start the server
+
   startApolloServer();
